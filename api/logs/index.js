92841facf4de45961354a2b81c6edd18a11f8897
asmalser <aaron@smalser.com>
2024-02-28 21:43:08 -0800

    Update index.js

--- api/logs/index.js
+++ api/logs/index.js
@@ -3,10 +3,38 @@ const got = require("got");
 module.exports = async (req, res, next) => {
   const { body, headers } = req;
 
+  if (!body || !Array.isArray(body)) {
+    return res.sendStatus(400);
+  }
+
+ // if (headers.authorization !== process.env.AUTH0_LOG_STREAM_TOKEN) {
+ //   return res.sendStatus(401);
+ // }
+
+  const failedLogs = body.filter((log) => {
+    return "f" === log.data.type[0] || /limit/.test(log.data.type);
+  });
+
+  if (failedLogs.length === 0) {
+    return res.sendStatus(204);
+  }
+
+  const reqOpts = {
+    json: {
+      attachments: failedLogs.map((log) => {
+        return {
+          pretext: "*Auth0 log alert*",
+          title: log.data.description + " [type: " + log.data.type + "]",
+          color: "#ff0000",
+          title_link: "https://manage.auth0.com/#/logs/" + log.data.log_id
+        };
+      }),
+    },
+  };
+
   try {
-    console.log( body );
+    console.log( JSON.stringify(reqOpts) );
   } catch (error) {
     next(error);
   }
-  
 };
