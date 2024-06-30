385904537477b63b613d799810dc83bc60295c76
asmalser <aaron@smalser.com>
2024-06-30 16:19:43 -0700

    Update index.js

--- api/logs/index.js
+++ api/logs/index.js
@@ -11,17 +11,17 @@ module.exports = async (req, res, next) => {
  //   return res.sendStatus(401);
  // }
 
-  const failedLogs = body.filter((log) => {
-    return "f" === log.data.type[0] || /limit/.test(log.data.type);
-  });
+  //const failedLogs = body.filter((log) => {
+  //  return "f" === log.data.type[0] || /limit/.test(log.data.type);
+  //});
 
-  if (failedLogs.length === 0) {
-    return res.sendStatus(204);
-  }
+  //if (failedLogs.length === 0) {
+  //  return res.sendStatus(204);
+  //}
 
   const reqOpts = {
     json: {
-      attachments: failedLogs.map((log) => {
+      attachments: body.map((log) => {
         return {
           pretext: "*Auth0 log alert*",
           title: log.data.description + " [type: " + log.data.type + "]",
